68c6f83bc6ab3f95c501169370789cab60fd5ff5
Jean de Dieu Uwimana <johnchafi@gmail.com>
2020-12-29 17:52:09 -0500

    Updates

---
+++ precache-manifest.f59361bcfc1f9bf342bec63fbd0b9b63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d4ad26c5b7cbf7ea4159353733c1ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d636f3d9581626696c1",
+    "url": "/static/js/2.88d5a06b.chunk.js"
+  },
+  {
+    "revision": "8d9af5dfc959e827ca47",
+    "url": "/static/js/main.6017bbf5.chunk.js"
+  },
+  {
+    "revision": "7387a786abccd29a8589",
+    "url": "/static/js/runtime~main.db5d0248.js"
+  },
+  {
+    "revision": "f5f9bf8a60ce118bd2621292b0959322",
+    "url": "/static/media/image.f5f9bf8a.png"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  }
+]);
